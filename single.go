5c10d3dc77d277cf95c633e34c57916e35566f22
abgr <bagher@raikaco.org>
2024-08-31 15:15:00 +0330

    add-single

---
+++ single.go
@@ -0,0 +1,13 @@
+package singleflight
+
+import (
+	"github.com/itsabgr/go-completer"
+)
+
+type Single[R any] struct {
+	flight Group[struct{}, R]
+}
+
+func (s *Single[R]) Do(fn func() R) completer.WaitContextFunc[R] {
+	return s.flight.Do(struct{}{}, fn)
+}
